b389fe46524349e6e02774a14bd02df0d6d5396a
Mabedallah <mohamed.altoom@gmail.com>
2024-02-22 21:47:04 +1100

    init the db, server and test

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const pool = require('./db')
+const port = 1337
+
+const app = express()
+
+app.get('/', (req,res)=>{
+    res.sendStatus(200)
+})
+
+
+
+app.listen(port, ()=> console.log(`server started on port: ${port}`))
