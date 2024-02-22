8ffcb35ecb80b486eb60b6430b6420ae467facba
Mabedallah <mohamed.altoom@gmail.com>
2024-02-22 22:08:46 +1100

    add some routs

--- server.js
+++ server.js
@@ -3,9 +3,37 @@ const pool = require('./db')
 const port = 1337
 
 const app = express()
+app.use(express.json())
 
-app.get('/', (req,res)=>{
+
+//Routs
+app.get('/', async(req,res)=>{
     res.sendStatus(200)
+})
+
+app.post('/', async(req,res)=>{
+    const {name , location} = req.body
+
+    try {
+        await pool.query('INSERT INTO schools (name,address) VALUES ($1,$2)',[name, location])
+        res.status(200).send({message: "Successfully added child"})
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+
+})
+
+app.get('/setup', async(req,res)=>{
+
+    try {
+        await pool.query('CREATE TABLE schools(id SERIAL PRIMARY KEY, name VARCHAR(100), address VARCHAR(100))')
+        res.status(200).send({message: "table created"})
+    } catch (error) {
+        console.log(error)
+        res.sendStatus(500)
+    }
+
 })
 
 
